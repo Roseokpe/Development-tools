eb6c19dedcbff3517a7041de7263bb41730524a7
Rose Okpe <roseokpe@gmail.com>
2022-09-14 15:24:13 -0700

    Update basis.sql

--- basis.sql
+++ basis.sql
@@ -1,3 +1,5 @@
 https://github.com/microverseinc/curriculum-databases/blob/main/db-structure/articles/sql_querying_data.md
 
 https://postgrescheatsheet.com/#/tables
+
+https://github.com/microverseinc/curriculum-databases/blob/main/db-structure/lessons/how_to_update_and_delete.md
